db5b628c3cd24a581190384bc0de38a722a0c521
suicanifuramu <suicanifuramu@gmail.com>
2025-01-12 11:18:55 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 #いろいろいんすこ
-sudo apt update
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install caddy -y
+
 sudo apt update
 sudo apt install tmux -y
 
